5adcb90a0a8e9c83e9498ef3cad045cb55896ff4
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2016-02-04 09:56:51 -0200

    Criado o exemplo da receita 100102 para o estado PR

--- exemplos/PR/Lote_r100110.php
+++ exemplos/PR/Lote_r100110.php
@@ -3,37 +3,21 @@ require_once "../../vendor/autoload.php";
 
 $guia = new \SGnre\Guia();
 $guia->c01_UfFavorecida = "PR";
-$guia->c02_receita = 100102;
+$guia->c02_receita = 100110;
 $guia->c27_tipoIdentificacaoEmitente = 1;
 $guia->c03_idContribuinteEmitente = '05698745000145';
-$guia->c28_tipoDocOrigem = '10';
-$guia->c04_docOrigem = '5712';
 $guia->c06_valorPrincipal = '10.00';
 $guia->c14_dataVencimento = '2016-02-20';
 $guia->c16_razaoSocialEmitente = 'Empresa Teste';
 $guia->c18_enderecoEmitente = 'Rua teste';
 $guia->c19_municipioEmitente = '17608';
 $guia->c20_ufEnderecoEmitente = 'RS';
+$guia->c21_cepEmitente = '95500000';
+$guia->c21_cepEmitente = '95500000';
 $guia->c33_dataPagamento = '2016-02-20';
-
-$extra = array(
-    0 => array(
-        'codigo' => 61,
-        'tipo' => 'T',
-        'valor' => 'teste de observacao'
-    ),
-    1 => array(
-        'codigo' => 65,
-        'tipo' => 'T',
-        'valor' => 'teste 2 de observacao'
-    ),
-    2 => array(
-        'codigo' => 66,
-        'tipo' => 'T',
-        'valor' => '43160205556927000126550010000027111533538812'
-    )
-);
-$guia->c39_camposExtras = $extra;
+$guia->periodo = '1';
+$guia->mes = '01';
+$guia->ano = '2016';
 
 $lote = new \SGnre\Lote();
 $lote->addGuia($guia);
